59d7e860269fbd4c309ce2e9e25ca1e175d093df
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-08-04 23:37:10 +0800

    revise style

--- js/aboutBox.js
+++ js/aboutBox.js
@@ -10,7 +10,7 @@ function fadeIn(box , opacity , r , g , b , o_dev) {
 }
 
 function fadeOut(box , opacity , r , g , b , o_dev) {
-    if (opacity > 0.001) {
+    if (opacity > 0.0001) {
         setTimeout(function() {
         opacity *= 0.8;
         box.style.opacity = opacity;
@@ -36,7 +36,7 @@ window.addEventListener("DOMContentLoaded" , function(){
     var close = document.getElementById("close");
     close.addEventListener('click' , function(){
         var opacity = 1;
-        var opacity_dev = 0.2;
+        var opacity_dev = 0.1;
         var r = g = b = 255;
         fadeOut(box , opacity , r , g , b , opacity_dev)
     })
