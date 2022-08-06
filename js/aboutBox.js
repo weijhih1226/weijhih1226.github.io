bc56724b0e4fe09d34e2d486643ae255179fd962
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-08-06 11:10:41 +0800

    revise about me

--- js/aboutBox.js
+++ js/aboutBox.js
@@ -1,21 +1,23 @@
-function fadeIn(block , opacity) {
+function fadeIn(block , box , opacity) {
     if (opacity < 1) {
         setTimeout(function() {
             opacity += 0.1;
             block.style.opacity = opacity;
             block.style.backgroundColor = "rgba(0, 0, 0," + (opacity / 2) + ")";
-            fadeIn(block , opacity)
+            box.style.marginTop = (1 - opacity) + "em";
+            fadeIn(block , box , opacity)
         } , 50)
     }
 }
 
-function fadeOut(block , opacity) {
+function fadeOut(block , box , opacity) {
     if (opacity > 0) {
         setTimeout(function() {
         opacity -= 0.1;
         block.style.opacity = opacity;
         block.style.backgroundColor = "rgba(0, 0, 0," + (opacity / 2) + ")";
-        fadeOut(block , opacity)
+        box.style.marginTop = (1 - opacity) + "em";
+        fadeOut(block , box , opacity)
         } , 50)
     }
     else {
@@ -25,17 +27,18 @@ function fadeOut(block , opacity) {
 
 window.addEventListener("DOMContentLoaded" , function(){
     var block = document.querySelector("#about_bg");
+    var box = document.querySelector("#about_block");
     var btn = document.querySelector("#about_btn");
     var close = document.querySelector("#close");
     btn.addEventListener('click' , function(){
         block.style.display = "block";
         // close.style.display = "block";
         var opacity = 0;
-        fadeIn(block , opacity)
+        fadeIn(block , box , opacity)
     })
     close.addEventListener('click' , function(){
         var opacity = 1;
-        fadeOut(block , opacity)
+        fadeOut(block , box , opacity)
     })
     // block.addEventListener('click' , function(){
     //     var opacity = 1;
