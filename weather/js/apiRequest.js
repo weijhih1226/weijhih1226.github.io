1ad42e3fff8964453f0a57b962145efec8458f5b
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-08-09 09:56:51 +0800

    revise windy

---
+++ weather/js/apiRequest.js
@@ -0,0 +1,25 @@
+const options = {
+    // Required: API key
+    key: 'x7Uj5Ay3gd9q2Qhuaxd4CtLIRJ24Lx3t', // REPLACE WITH YOUR KEY !!!
+
+    // Put additional console output
+    verbose: true,
+
+    // Optional: Initial state of the map
+    lat: 23.8,
+    lon: 121,
+    zoom: 5,
+};
+
+windyInit(options, windyAPI => {
+    // windyAPI is ready, and contain 'map', 'store',
+    // 'picker' and other usefull stuff
+
+    const { map } = windyAPI;
+    // .map is instance of Leaflet map
+
+    L.popup()
+        .setLatLng([50.4, 14.3])
+        .setContent('Hello World')
+        .openOn(map);
+});
