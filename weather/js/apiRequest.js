ad5b4f48404d0576c3ac92d45a1d6e86278a4733
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-08-09 11:35:55 +0800

    revise map

--- weather/js/apiRequest.js
+++ weather/js/apiRequest.js
@@ -24,67 +24,63 @@ windyInit(options, windyAPI => {
         .openOn(map);
 });
 
-// function gettingPosition(){
-//     if(navigator.geolocation){
-//         return new Promise((resolve, reject) => {
-//             let option = {
-//                 enableAcuracy:false, // 提高精確度
-//                 maximumAge:0, // 設定上一次位置資訊的有效期限(毫秒)
-//                 timeout:10000 // 逾時計時器(毫秒)
-//             };
-//             navigator.geolocation.getCurrentPosition(resolve, reject, option);
-//         })
-//     }else{
-//         alert("Does not support positioning!");
-//     }
-// }
-// function successCallback(position){
-//     console.log(position);
-// }
-// function errorCallback(error) {
-//     alert(error.message); //error.code
-// }
+function gettingPosition(){
+    if(navigator.geolocation){
+        return new Promise((resolve, reject) => {
+            let option = {
+                enableAcuracy:false, // 提高精確度
+                maximumAge:0, // 設定上一次位置資訊的有效期限(毫秒)
+                timeout:10000 // 逾時計時器(毫秒)
+            };
+            navigator.geolocation.getCurrentPosition(resolve, reject, option);
+        })
+    }else{
+        alert("Does not support positioning!");
+    }
+}
+function successCallback(position){
+    console.log(position);
+}
+function errorCallback(error) {
+    alert(error.message); //error.code
+}
 
-// gettingPosition()
-// .then(position => successCallback(position))
-// .catch(error => errorCallback(error))
+function mapCenterControl(map, clickButton) {
+    let centerControlDiv = document.createElement('div');
 
-// function mapCenterControl(map, clickButton) {
-//     let centerControlDiv = document.createElement('div');
+    // Set CSS for the control border.
+    let controlUI = document.createElement('button');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = 'none';
+    controlUI.style.outline = 'none';
+    controlUI.style.width = '40px';
+    controlUI.style.height = '40px';
+    controlUI.style.borderRadius = '2px';
+    controlUI.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginRight = '10px';
+    controlUI.style.padding = '0';
+    controlUI.title = 'Your Location';
+    centerControlDiv.appendChild(controlUI);
 
-//     // Set CSS for the control border.
-//     let controlUI = document.createElement('button');
-//     controlUI.style.backgroundColor = '#fff';
-//     controlUI.style.border = 'none';
-//     controlUI.style.outline = 'none';
-//     controlUI.style.width = '40px';
-//     controlUI.style.height = '40px';
-//     controlUI.style.borderRadius = '2px';
-//     controlUI.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
-//     controlUI.style.cursor = 'pointer';
-//     controlUI.style.marginRight = '10px';
-//     controlUI.style.padding = '0';
-//     controlUI.title = 'Your Location';
-//     centerControlDiv.appendChild(controlUI);
+    // Set CSS for the control interior.
+    let controlText = document.createElement('div');
+    controlText.style.margin = '10px';
+    controlText.style.width = '30px';
+    controlText.style.height = '30px';
+    controlText.style.backgroundImage = 'url(\'../image/icon/location-pin.svg\')';
+    controlText.style.backgroundSize = '20px 20px';
+    controlText.style.backgroundPosition = '0px 0px';
+    controlText.style.backgroundRepeat = 'no-repeat';
+    controlUI.appendChild(controlText);
 
-//     // Set CSS for the control interior.
-//     let controlText = document.createElement('div');
-//     controlText.style.margin = '10px';
-//     controlText.style.width = '30px';
-//     controlText.style.height = '30px';
-//     controlText.style.backgroundImage = 'url(\'../image/icon/location-pin.svg\')';
-//     controlText.style.backgroundSize = '20px 20px';
-//     controlText.style.backgroundPosition = '0px 0px';
-//     controlText.style.backgroundRepeat = 'no-repeat';
-//     controlUI.appendChild(controlText);
+    // constructor passing in this DIV.
+    centerControlDiv.index = 1; // 排列優先度
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv); // 設定按鈕加入地圖的位置
 
-//     // constructor passing in this DIV.
-//     centerControlDiv.index = 1; // 排列優先度
-//     map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv); // 設定按鈕加入地圖的位置
-
-//     // Setup the click event listeners.
-//     controlUI.addEventListener('click', () => clickButton());
-// }
+    // Setup the click event listeners.
+    controlUI.addEventListener('click', () => clickButton());
+}
 
 function initMap() {
     var center = {lat: 23.8, lng: 121};
@@ -96,9 +92,9 @@ function initMap() {
       position: center,
       map: map
     });
-    // mapCenterControl(map,
-	// 	() => gettingPosition()
-	// 	.then(position => successCallback(position))
-	//     .catch(error => errorCallback(error))
-    // )
+    mapCenterControl(map,
+		() => gettingPosition()
+		.then(position => successCallback(position))
+	    .catch(error => errorCallback(error))
+    )
 }
