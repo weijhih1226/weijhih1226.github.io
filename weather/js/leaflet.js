6aca492ca65d3a1e514271edfb66f90f6ca11a83
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-08-11 00:45:39 +0800

    add rain layer

--- weather/js/leaflet.js
+++ weather/js/leaflet.js
@@ -1,4 +1,11 @@
 window.addEventListener("DOMContentLoaded" , function(){
+    // 放置地圖
+    let map = L.map('map' , {
+        zoom: 7, // 0 - 18
+        center: [23.8, 121], // 中心點座標
+        preferCanvas: true // recommended when loading large layers.
+    })
+    
     // 設定圖層來源
     const baseOpenStreet = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap', // 商用時必須要有版權出處
@@ -17,6 +24,7 @@ window.addEventListener("DOMContentLoaded" , function(){
         subdomains:['mt0','mt1','mt2','mt3']
     });
     const kmlRainUrl = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/DIV2/O-A0040-003.kmz';
+    // const kmlRainUrl = 'kmz/PNWAshLocations.kmz';
     const imgRadarUrl = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-005.png';
     // const imgRadarBounds = [[17.992071044171471, 115.001445629639946], [29.004257649173013, 126.514775012745119]];
     // const imgRadarBounds = [[17.9875, 114.9875], [29.0125, 126.5125]];
@@ -33,27 +41,20 @@ window.addEventListener("DOMContentLoaded" , function(){
     };
     let overlays = {'雷達': radar};
 
-    // *** 放置地圖
-    let map = L.map('map' , {
-        zoom: 7, // 0 - 18
-        center: [23.8, 121], // 中心點座標
-    })
-
     // Instantiate KMZ layer (async)
     var kmz = L.kmzLayer().addTo(map);
 
     kmz.on('load', function(e) {
+        e.name = '雨量';
         control.addOverlay(e.layer, e.name);
-        // e.layer.addTo(map);
+        e.layer.addTo(map);
     });
 
     // Add remote KMZ files as layers (NB if they are 3rd-party servers, they MUST have CORS enabled)
-    kmz.load('https://raruto.github.io/leaflet-kmz/examples/regions.kmz');
-    kmz.load('https://raruto.github.io/leaflet-kmz/examples/capitals.kmz');
-    kmz.load('https://raruto.github.io/leaflet-kmz/examples/globe.kmz');
+    kmz.load(kmlRainUrl)
 
     baseLayers['衛星'].addTo(map); // 使用中文地圖作為預設
     radar.addTo(map);
     
-    L.control.layers(baseLayers , overlays).addTo(map); // 加入地圖切換控制項
+    var control = L.control.layers(baseLayers , overlays , { collapsed:false }).addTo(map); // 加入地圖切換控制項
 })
